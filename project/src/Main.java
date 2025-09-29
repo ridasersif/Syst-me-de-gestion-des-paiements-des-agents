e0ccd7df4184d441e8b2ee4d2151d501d7bd13e7
ridasersif <ridasersif1@gmail.com>
2025-09-29 15:01:21 +0200

    🔹 Modifications dans Main.java

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,33 +1,10 @@
-import config.DatabaseConnection;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import view.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
-
-
-            try {
-                Connection conn = DatabaseConnection.getConnection();
-                Statement stmt = conn.createStatement();
-
-                String sql = "SELECT * FROM agents";
-                ResultSet rs = stmt.executeQuery(sql);
-
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String name = rs.getString("name");
-                    String dept = rs.getString("department");
-                    double salary = rs.getDouble("salary");
-
-                    System.out.println(id + " | " + name + " | " + dept + " | " + salary);
-                }
-
-            } catch (SQLException e) {
-                System.out.println("Database connection failed: " + e.getMessage());
-            }
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.show();
+        //1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣
 
     }
-
 }
